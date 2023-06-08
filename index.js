137baa2c46a0c1b1fe0b3ad7117962634b007452
Saddam Azy <myxzlpltk@gmail.com>
2023-06-08 13:34:59 +0700

    change trigger to firestore

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
-const functions = require('@google-cloud/functions-framework');
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
 const { encode } = require('blurhash')
-const crypto = require('crypto');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const { Canvas, loadImage } = require('canvas');
+const { Canvas, loadImage } = require('canvas')
 const { Storage } = require("@google-cloud/storage")
-const { Firestore } = require('@google-cloud/firestore');
+const { Firestore } = require('@google-cloud/firestore')
 
 // Create a new client
-const firestore = new Firestore({ projectId: "cosmic-anthem-386408" });
-const gcs = new Storage({ projectId: "cosmic-anthem-386408" });
+const firestore = new Firestore({ projectId: "cosmic-anthem-386408" })
+const gcs = new Storage({ projectId: "cosmic-anthem-386408" })
 
 /**
- * Triggered from a change to a Cloud Storage bucket.
+ * Triggered by a change to a Firestore document.
  *
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
-functions.cloudEvent("encodeBlurHash", async (event, context) => {
-  // TODO: Remove this when deploy
-  event = event.data
+exports.encodeBlurHash = async (event, context) => {
+  console.log("Processing: ", JSON.stringify(context.resource))
+
+  // Get resource
+  const resource = event.value.name
+  const id = event.value.fields.filename.stringValue
+  const affectedDoc = firestore.doc(resource.split('/documents/')[1])
 
   // Get file bucket
-  const bucket = gcs.bucket(event.bucket);
-  const blob = bucket.file(event.name);
+  const bucket = gcs.bucket("cs23-ps414-images-bkt")
+  const blob = bucket.file("images/" + id)
 
   // Generate random filename
-  const randomFileName = crypto.randomBytes(20).toString('hex');
-  const tempLocalFile = path.join(os.tmpdir(), randomFileName);
+  const tempLocalFile = path.join(os.tmpdir(), id)
 
   // Download image
-  await blob.download({ destination: tempLocalFile });
+  await blob.download({ destination: tempLocalFile })
 
   // Set canvas
-  const imageWidth = 224;
-  const imageHeight = 224;
-  const canvas = new Canvas(imageWidth, imageHeight);
-  const canvasCtx = canvas.getContext('2d');
+  const imageWidth = 224
+  const imageHeight = 224
+  const canvas = new Canvas(imageWidth, imageHeight)
+  const canvasCtx = canvas.getContext('2d')
 
   // Draw canvas
-  const myImg = await loadImage(tempLocalFile);
-  canvasCtx.drawImage(myImg, 0, 0);
+  const myImg = await loadImage(tempLocalFile)
+  canvasCtx.drawImage(myImg, 0, 0)
 
   // Get canvas data
-  const imageData = canvasCtx.getImageData(0, 0, imageWidth, imageHeight);
+  const imageData = canvasCtx.getImageData(0, 0, imageWidth, imageHeight)
 
   // Get blurhash
-  const hash = encode(imageData.data, imageWidth, imageHeight, 3, 3);
+  const hash = encode(imageData.data, imageWidth, imageHeight, 3, 3)
 
   // Save data
-  const doc_id = path.basename(event.name)
-  const doc_ref = firestore.collection("images").doc(doc_id)
-  doc_ref.update({ 'blurHash': hash })
+  affectedDoc.update({ 'blurHash': hash })
 
   // Cleanup
   fs.unlinkSync(tempLocalFile)
-})
+}
